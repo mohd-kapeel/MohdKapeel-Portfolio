079bfa4b6fbaa3df1ede480e2a9a6f34ff885640
Mohd Kapeel <mohdkapeel786@gmail.com>
2026-08-12 21:42:01 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -518,7 +518,7 @@ function App() {
             <div className="orbit-corner orbit-top-right" />
             <div className="orbit-corner orbit-bottom-left" />
             <div className="photo-circle">
-              <img src="/kapeel-profile.png" alt="Mohd Kapeel portrait" />
+              <img src="/professional.jpg" alt="Mohd Kapeel portrait" />
             </div>
           </div>
         </div>
